cef753880359c700aa1b41634b36f74361eac858
UillianLopes <uilliansl@outlook.com>
2022-08-15 19:12:13 -0300

    Fix

--- POne.Core/Auth/Roles.cs
+++ POne.Core/Auth/Roles.cs
@@ -32,7 +32,43 @@ namespace POne.Core.Auth
     [AttributeUsage(AttributeTargets.Field)]
     class RoleActionAttribute : RoleAttribute
     {
-        public RoleActionAttribute() : base()
+        public string I18nKey { get; set; }
+
+        public RoleActionAttribute(string i18nKey)
+        {
+            I18nKey = i18nKey;
+        }
+    }
+
+    [AttributeUsage(AttributeTargets.Field)]
+    class CreateActionAttribute : RoleActionAttribute
+    {
+        public CreateActionAttribute() : base("@PONE.AUTH_APPS.ROLES.CREATE")
+        {
+        }
+    }
+
+
+    [AttributeUsage(AttributeTargets.Field)]
+    class UpdateActionAttribute : RoleActionAttribute
+    {
+        public UpdateActionAttribute() : base("@PONE.AUTH_APPS.ROLES.UPDATE")
+        {
+        }
+    }
+
+    [AttributeUsage(AttributeTargets.Field)]
+    class DeleteActionAttribute : RoleActionAttribute
+    {
+        public DeleteActionAttribute() : base("@PONE.AUTH_APPS.ROLES.DELETE")
+        {
+        }
+    }
+
+    [AttributeUsage(AttributeTargets.Field)]
+    class ReadActionAttribute : RoleActionAttribute
+    {
+        public ReadActionAttribute() : base("@PONE.AUTH_APPS.ROLES.READ")
         {
         }
     }
@@ -70,148 +106,6 @@ namespace POne.Core.Auth
         public List<AuthModule> Modules { get; private set; }
     }
 
-    public class RolesUtils
-    {
-        static IEnumerable<string> GetI18nKeysFromType(MemberInfo member, string prefix)
-        {
-
-            if (member is FieldInfo fieldInfo)
-            {
-                if (fieldInfo.GetCustomAttribute<RoleActionAttribute>() is RoleActionAttribute appAttribute)
-                {
-                    var newPrefix = $"{prefix}.ROLES.{appAttribute.I18n ?? fieldInfo.Name.ToUpper()}";
-                    yield return $"{newPrefix}.TITLE";
-                    yield return $"{newPrefix}.DESCRIPTION";
-                }
-            }
-            else if (member is Type memberType && memberType.IsClass)
-            {
-                if (memberType.GetCustomAttribute<RoleAppAttribute>() is RoleAppAttribute appAttribute)
-                {
-                    var newPrefix = $"{prefix}.{appAttribute.I18n ?? memberType.Name.ToUpper()}";
-                    yield return $"{newPrefix}.TITLE";
-                    yield return $"{newPrefix}.DESCRIPTION";
-
-                    foreach (var fieldMember in memberType.GetFields())
-                        foreach (var role in GetI18nKeysFromType(fieldMember, newPrefix))
-                            yield return role;
-
-                    foreach (var nestedType in memberType.GetNestedTypes())
-                        foreach (var role in GetI18nKeysFromType(nestedType, newPrefix))
-                            yield return role;
-
-                }
-                else if (memberType.GetCustomAttribute<RoleModuleAttribute>() is RoleModuleAttribute moduleAttribute)
-                {
-                    var newPrefix = $"{prefix}.MODULES.{moduleAttribute.I18n ?? memberType.Name.ToUpper()}";
-                    yield return $"{newPrefix}.TITLE";
-                    yield return $"{newPrefix}.DESCRIPTION";
-
-                    foreach (var fieldMember in memberType.GetFields())
-                        foreach (var role in GetI18nKeysFromType(fieldMember, newPrefix))
-                            yield return role;
-
-                    foreach (var nestedType in memberType.GetNestedTypes())
-                        foreach (var role in GetI18nKeysFromType(nestedType, newPrefix))
-                            yield return role;
-                }
-            }
-
-            yield break;
-        }
-
-        public static IEnumerable<string> GenerateI18nKeysBasedOnRoles()
-        {
-            foreach (var memberType in typeof(Roles).GetNestedTypes())
-                foreach (var value in GetI18nKeysFromType(memberType, "@PONE.AUTH_APPS"))
-                    yield return value;
-        }
-
-        static AuthRole GetAction(FieldInfo member, string prefix)
-        {
-            if (member.GetCustomAttribute<RoleActionAttribute>() is not RoleActionAttribute roleAppAttribute)
-                return null;
-
-            var action = new AuthRole();
-            var newPrefix = $"{prefix}.ROLES.{roleAppAttribute.I18n ?? member.Name.ToUpper()}";
-            action.Description = $"{newPrefix}.DESCRIPTION";
-            action.Title = $"{newPrefix}.TITLE";
-            action.Key = (string)member.GetValue(null);
-
-            return action;
-        }
-
-        static AuthModule GetModule(MemberInfo member, string prefix)
-        {
-            if (member is not Type memberType || memberType.GetCustomAttribute<RoleModuleAttribute>() is not RoleModuleAttribute roleAppAttribute)
-                return null;
-
-            var module = new AuthModule();
-            var newPrefix = $"{prefix}.MODULES.{roleAppAttribute.I18n ?? member.Name.ToUpper()}";
-            module.Description = $"{newPrefix}.DESCRIPTION";
-            module.Title = $"{newPrefix}.TITLE";
-
-            foreach (var fieldInfo in memberType.GetFields())
-                if (GetAction(fieldInfo, newPrefix) is AuthRole jsonAction)
-                    module.Roles.Add(jsonAction);
-
-            return module;
-        }
-
-        static AuthApp GetApp(MemberInfo member, string prefix)
-        {
-            if (member is not Type memberType || memberType.GetCustomAttribute<RoleAppAttribute>() is not RoleAppAttribute roleAppAttribute)
-                return null;
-
-            var app = new AuthApp();
-            var newPrefix = $"{prefix}.{roleAppAttribute.I18n ?? member.Name.ToUpper()}";
-            app.Description = $"{newPrefix}.DESCRIPTION";
-            app.Title = $"{newPrefix}.TITLE";
-
-            foreach (var fieldInfo in memberType.GetNestedTypes())
-                if (GetModule(fieldInfo, newPrefix) is AuthModule jsonModule)
-                    app.Modules.Add(jsonModule);
-
-            return app;
-        }
-
-        public static IEnumerable<AuthApp> ReadAppsAndRoles()
-        {
-            foreach (var memberType in typeof(Roles).GetNestedTypes())
-                yield return GetApp(memberType, "@PONE.AUTH_APPS");
-        }
-
-        public static ISet<string> GetAllRoleKeys()
-        {
-            var rolesSet = new SortedSet<string>();
-
-            foreach (var appType in typeof(Roles).GetNestedTypes())
-            {
-                if (appType.GetCustomAttribute<RoleAppAttribute>() is not RoleAppAttribute roleApp)
-                    continue;
-
-                foreach (var moduleType in appType.GetNestedTypes())
-                {
-                    if (moduleType.GetCustomAttribute<RoleModuleAttribute>() is not RoleModuleAttribute roleModule)
-                        continue;
-
-                    foreach (var roleField in moduleType.GetFields())
-                    {
-                        if (roleField.GetCustomAttribute<RoleActionAttribute>() is not RoleActionAttribute roleAction)
-                            continue;
-
-                        var role = (string)roleField.GetValue(null);
-
-                        rolesSet.Add(role);
-                    }
-
-                }
-            }
-
-            return rolesSet;
-        }
-    }
-
     public sealed class Roles
     {
         [RoleApp]
@@ -220,32 +114,32 @@ namespace POne.Core.Auth
             [RoleModule]
             public sealed class User
             {
-                [RoleAction]
+                [CreateAction]
                 public const string Create = "ADMIN_USER_CREATE";
 
-                [RoleAction]
+                [UpdateAction]
                 public const string Update = "ADMIN_USER_UPDATE";
 
-                [RoleAction]
+                [DeleteAction]
                 public const string Delete = "ADMIN_USER_DELETE";
 
-                [RoleAction]
+                [ReadAction]
                 public const string Read = "ADMIN_USER_READ";
             }
 
             [RoleModule]
             public sealed class Profile
             {
-                [RoleAction]
+                [CreateAction]
                 public const string Create = "ADMIN_PROFILE_CREATE";
 
-                [RoleAction]
+                [UpdateAction]
                 public const string Update = "ADMIN_PROFILE_UPDATE";
 
-                [RoleAction]
+                [DeleteAction]
                 public const string Delete = "ADMIN_PROFILE_DELETE";
 
-                [RoleAction]
+                [ReadAction]
                 public const string Read = "ADMIN_PROFILE_READ";
             }
         }
@@ -256,96 +150,96 @@ namespace POne.Core.Auth
             [RoleModule]
             public sealed class Bank
             {
-                [RoleAction]
+                [CreateAction]
                 public const string Create = "FINANCIAL_BANK_CREATE";
 
-                [RoleAction]
+                [UpdateAction]
                 public const string Update = "FINANCIAL_BANK_UPDATE";
 
-                [RoleAction]
+                [DeleteAction]
                 public const string Delete = "FINANCIAL_BANK_DELETE";
 
-                [RoleAction]
+                [ReadAction]
                 public const string Read = "FINANCIAL_BANK_READ";
             }
 
             [RoleModule]
             public sealed class Category
             {
-                [RoleAction]
+                [CreateAction]
                 public const string Create = "FINANCIAL_CATEGORY_CREATE";
 
-                [RoleAction]
+                [UpdateAction]
                 public const string Update = "FINANCIAL_CATEGORY_UPDATE";
 
-                [RoleAction]
+                [DeleteAction]
                 public const string Delete = "FINANCIAL_CATEGORY_DELETE";
 
-                [RoleAction]
+                [ReadAction]
                 public const string Read = "FINANCIAL_CATEGORY_READ";
             }
 
             [RoleModule]
             public sealed class Dashboard
             {
-                [RoleAction]
+                [CreateAction]
                 public const string Create = "FINANCIAL_DASHBOARD_CREATE";
 
-                [RoleAction]
+                [UpdateAction]
                 public const string Update = "FINANCIAL_DASHBOARD_UPDATE";
 
-                [RoleAction]
+                [DeleteAction]
                 public const string Delete = "FINANCIAL_DASHBOARD_DELETE";
 
-                [RoleAction]
+                [ReadAction]
                 public const string Read = "FINANCIAL_DASHBOARD_READ";
             }
 
             [RoleModule]
             public sealed class Entry
             {
-                [RoleAction]
+                [CreateAction]
                 public const string Create = "FINANCIAL_ENTRY_CREATE";
 
-                [RoleAction]
+                [UpdateAction]
                 public const string Update = "FINANCIAL_ENTRY_UPDATE";
 
-                [RoleAction]
+                [DeleteAction]
                 public const string Delete = "FINANCIAL_ENTRY_DELETE";
 
-                [RoleAction]
+                [ReadAction]
                 public const string Read = "FINANCIAL_ENTRY_READ";
             }
 
             [RoleModule]
             public sealed class SubCategory
             {
-                [RoleAction]
+                [CreateAction]
                 public const string Create = "FINANCIAL_SUB_CATEGORY_CREATE";
 
-                [RoleAction]
+                [UpdateAction]
                 public const string Update = "FINANCIAL_SUB_CATEGORY_UPDATE";
 
-                [RoleAction]
+                [DeleteAction]
                 public const string Delete = "FINANCIAL_SUB_CATEGORY_DELETE";
 
-                [RoleAction]
+                [ReadAction]
                 public const string Read = "FINANCIAL_SUB_CATEGORY_READ";
             }
 
             [RoleModule]
             public sealed class Wallet
             {
-                [RoleAction]
+                [CreateAction]
                 public const string Create = "FINANCIAL_WALLET_CREATE";
 
-                [RoleAction]
+                [UpdateAction]
                 public const string Update = "FINANCIAL_WALLET_UPDATE";
 
-                [RoleAction]
+                [DeleteAction]
                 public const string Delete = "FINANCIAL_WALLET_DELETE";
 
-                [RoleAction]
+                [ReadAction]
                 public const string Read = "FINANCIAL_WALLET_READ";
             }
         }
---
+++ POne.Core/Auth/RolesUtils.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+
+namespace POne.Core.Auth
+{
+
+    public class RolesUtils
+    {
+        static IEnumerable<string> GetI18nKeysFromType(MemberInfo member, string prefix)
+        {
+
+            if (member is FieldInfo fieldInfo)
+            {
+                if (fieldInfo.GetCustomAttribute<RoleActionAttribute>() is RoleActionAttribute actionAttribute)
+                {
+                    var newPrefix = $"{prefix}.ROLES.{fieldInfo.Name.ToUpper()}";
+                    yield return actionAttribute.I18nKey ?? $"{newPrefix}.TITLE";
+                    yield return $"{newPrefix}.DESCRIPTION";
+                }
+            }
+            else if (member is Type memberType && memberType.IsClass)
+            {
+                if (memberType.GetCustomAttribute<RoleAppAttribute>() is RoleAppAttribute appAttribute)
+                {
+                    var newPrefix = $"{prefix}.{appAttribute.I18n ?? memberType.Name.ToUpper()}";
+                    yield return $"{newPrefix}.TITLE";
+                    yield return $"{newPrefix}.DESCRIPTION";
+
+                    foreach (var fieldMember in memberType.GetFields())
+                        foreach (var role in GetI18nKeysFromType(fieldMember, newPrefix))
+                            yield return role;
+
+                    foreach (var nestedType in memberType.GetNestedTypes())
+                        foreach (var role in GetI18nKeysFromType(nestedType, newPrefix))
+                            yield return role;
+
+                }
+                else if (memberType.GetCustomAttribute<RoleModuleAttribute>() is RoleModuleAttribute moduleAttribute)
+                {
+                    var newPrefix = $"{prefix}.MODULES.{moduleAttribute.I18n ?? memberType.Name.ToUpper()}";
+                    yield return $"{newPrefix}.TITLE";
+                    yield return $"{newPrefix}.DESCRIPTION";
+
+                    foreach (var fieldMember in memberType.GetFields())
+                        foreach (var role in GetI18nKeysFromType(fieldMember, newPrefix))
+                            yield return role;
+
+                    foreach (var nestedType in memberType.GetNestedTypes())
+                        foreach (var role in GetI18nKeysFromType(nestedType, newPrefix))
+                            yield return role;
+                }
+            }
+
+            yield break;
+        }
+
+        public static IEnumerable<string> GenerateI18nKeysBasedOnRoles()
+        {
+            foreach (var memberType in typeof(Roles).GetNestedTypes())
+                foreach (var value in GetI18nKeysFromType(memberType, "@PONE.AUTH_APPS"))
+                    yield return value;
+        }
+
+        static AuthRole GetAction(FieldInfo member, string prefix)
+        {
+            if (member.GetCustomAttribute<RoleActionAttribute>() is not RoleActionAttribute roleAppAttribute)
+                return null;
+
+            var action = new AuthRole();
+            var newPrefix = $"{prefix}.ROLES.{roleAppAttribute.I18n ?? member.Name.ToUpper()}";
+            action.Description = $"{newPrefix}.DESCRIPTION";
+            action.Title = roleAppAttribute.I18nKey ?? $"{newPrefix}.TITLE";
+            action.Key = (string)member.GetValue(null);
+
+            return action;
+        }
+
+        static AuthModule GetModule(MemberInfo member, string prefix)
+        {
+            if (member is not Type memberType || memberType.GetCustomAttribute<RoleModuleAttribute>() is not RoleModuleAttribute roleAppAttribute)
+                return null;
+
+            var module = new AuthModule();
+            var newPrefix = $"{prefix}.MODULES.{roleAppAttribute.I18n ?? member.Name.ToUpper()}";
+            module.Description = $"{newPrefix}.DESCRIPTION";
+            module.Title = $"{newPrefix}.TITLE";
+
+            foreach (var fieldInfo in memberType.GetFields())
+                if (GetAction(fieldInfo, newPrefix) is AuthRole jsonAction)
+                    module.Roles.Add(jsonAction);
+
+            return module;
+        }
+
+        static AuthApp GetApp(MemberInfo member, string prefix)
+        {
+            if (member is not Type memberType || memberType.GetCustomAttribute<RoleAppAttribute>() is not RoleAppAttribute roleAppAttribute)
+                return null;
+
+            var app = new AuthApp();
+            var newPrefix = $"{prefix}.{roleAppAttribute.I18n ?? member.Name.ToUpper()}";
+            app.Description = $"{newPrefix}.DESCRIPTION";
+            app.Title = $"{newPrefix}.TITLE";
+
+            foreach (var fieldInfo in memberType.GetNestedTypes())
+                if (GetModule(fieldInfo, newPrefix) is AuthModule jsonModule)
+                    app.Modules.Add(jsonModule);
+
+            return app;
+        }
+
+        public static IEnumerable<AuthApp> ReadAppsAndRoles()
+        {
+            foreach (var memberType in typeof(Roles).GetNestedTypes())
+                yield return GetApp(memberType, "@PONE.AUTH_APPS");
+        }
+
+        public static ISet<string> GetAllRoleKeys()
+        {
+            var rolesSet = new SortedSet<string>();
+
+            foreach (var appType in typeof(Roles).GetNestedTypes())
+            {
+                if (appType.GetCustomAttribute<RoleAppAttribute>() is not RoleAppAttribute roleApp)
+                    continue;
+
+                foreach (var moduleType in appType.GetNestedTypes())
+                {
+                    if (moduleType.GetCustomAttribute<RoleModuleAttribute>() is not RoleModuleAttribute roleModule)
+                        continue;
+
+                    foreach (var roleField in moduleType.GetFields())
+                    {
+                        if (roleField.GetCustomAttribute<RoleActionAttribute>() is not RoleActionAttribute roleAction)
+                            continue;
+
+                        var role = (string)roleField.GetValue(null);
+
+                        rolesSet.Add(role);
+                    }
+
+                }
+            }
+
+            return rolesSet;
+        }
+    }
+}
